483e1c5dcd57f8d3dd3e47dc09183b33daeb0b98
mrspaar <julienpistre.cagnes@gmail.com>
2022-09-02 00:06:43 +0200

    Replaced class with object

--- jda/src/main/kotlin/Global.kt
+++ jda/src/main/kotlin/Global.kt
@@ -2,20 +2,16 @@ import io.github.cdimascio.dotenv.dotenv
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction
 
-class Vars {
-    companion object {
-        private val dotenv = dotenv()
-        val DISCORD_TOKEN = dotenv["DISCORD_TOKEN"]!!
-        val DB_URI = dotenv["DB_URI"]!!
-    }
+object Vars {
+    private val dotenv = dotenv()
+    val DISCORD_TOKEN = dotenv["DISCORD_TOKEN"]!!
+    val DB_URI = dotenv["DB_URI"]!!
 }
 
-class Colors {
-    companion object {
-        const val GREEN = 0x2ECC71
-        const val ORANGE = 0xC27C0E
-        const val RED = 0x3498DB
-    }
+object Colors {
+    const val GREEN = 0x2ECC71
+    const val ORANGE = 0xC27C0E
+    const val RED = 0x3498DB
 }
 
 fun replyEmbed(interaction: SlashCommandInteraction, color: Int, description: String) {
